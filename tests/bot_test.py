48c72bf4ce6e8d9bc2674b340fd214c603cb2a8c
eloy <eloy.colell@gmail.com>
2015-08-05 13:43:49 -0300

    It refactored the bot.

---
+++ tests/bot_test.py
@@ -0,0 +1,29 @@
+import unittest
+from populartwitterbot import Bot
+import json
+from datetime import datetime
+import hashlib
+import time
+
+
+class TestBot(unittest.TestCase):
+
+    def setUp(self):
+        with open('config.json') as f:
+            config = json.load(f).items()[0]
+        self.bot = Bot(config)
+        self.text = 'Testing from github.com/limiear/populartwitterbot.'
+        self.text += (" (%s)" % hashlib.md5(str(datetime.now())).
+                      digest().encode("base64")[0:5].lower())
+
+    def test_name(self):
+        # self.assertEquals(self.bot.name, 'populartwitterbot00')
+        self.bot.populartwitterbot00.update_status(status=self.text)
+        time.sleep(10)
+
+    def tearDown(self):
+        self.bot.shutdown()
+
+
+if __name__ == '__main__':
+    unittest.main()
