a9ef95d8da71b027cae1b7f1a8a57007b396a576
eloy <eloy.colell@gmail.com>
2015-08-05 13:37:10 -0300

    It refactor some code.

--- populartwitterbot/bot.py
+++ populartwitterbot/bot.py
@@ -2,32 +2,110 @@ from twython import Twython
 import logging
 import json
 from limits import LimitManager
+from queuelib import FifoDiskQueue
+import pickle
 
 
 class Access(object):
 
-    def __init__(self, config, enabled=True):
+    def __init__(self, config, limits):
         self.config = config
-        self.twitter = Twython(
+        self.api = Twython(
             config['api_key'],
             config['api_secret'],
             config['access'],
             config['access_secret'])
-        print self.name, enabled
-        self.limits = LimitManager(self, enabled)
+        self.observe(limits)
+        self.priority_order = self.limits.keys()
+        self.initialize_queues()
+        self.wrap(self.api)
+
+    def observe(self, limits):
+        self.limits = limits
+        self.limits.register(self)
+
+    def wrap(self, api):
+        enabled = self.config['enabled']
+        self._get = api.get if enabled else self.fake_call
+        self._post = api.post if enabled else self.fake_call
+        api.get = self.get
+        api.post = self.post
+
+    def unwrap(self, api):
+        api.get = self._get
+        api.post = self._post
+
+    def initialize_queues(self):
+        self.qfactory = (lambda endpoint:
+                         FifoDiskQueue('./collections/%s' %
+                                       endpoint.replace('/', '+')))
+        self.queues = dict(map(lambda k: (k, self.qfactory(k)),
+                               self.limits.keys()))
+
+    def pop(self, queue_name):
+        request = self.queues[queue_name].pop()
+        if request:
+            request = pickle.loads(request)
+        return request
+
+    def push(self, params):
+        endpoint = params[1]
+        if endpoint not in self.queues:
+            self.queues[endpoint] = self.qfactory(endpoint)
+            self.priority_order.append(endpoint)
+        self.queues[endpoint].push(pickle.dumps(params))
+
+    def get(self, endpoint, params=None, version='1.1'):
+        self.push(['get', endpoint, params, version])
+
+    def post(self, endpoint, params=None, version='1.1'):
+        self.push(['post', endpoint, params, version])
+
+    def fake_call(self, *args, **kwargs):
+        logging.info(args, kwargs)
 
     @property
     def name(self):
         return self.config['app_name']
 
+    def send(self, request):
+        return getattr(self, '_%s' % request[0])(
+            request[1],
+            request[2],
+            request[3])
+
     def __getattr__(self, name):
-        return getattr(self.twitter, name)
+        return getattr(self.api, name)
 
     def close(self):
-        self.limits.shutdown()
+        for queue in self.queues.values():
+            queue.close()
+        self.unwrap(self.api)
 
 
 class Bot(object):
+
+    def __init__(self, config):
+        self.name = config[0]
+        self.config = config[1]
+        self.limits = LimitManager()
+        self.access = dict(map(lambda c:
+                               (c['app_name'], Access(c, self.limits)),
+                               self.config['oauth']))
+        self.limits.bootup_engine()
+
+    def shutdown(self):
+        self.limits.shutdown_engine()
+
+    def __getattr__(self, name):
+        if name in self.access:
+            return self.access[name]
+        else:
+            default = self.access.items()[0][1]
+            return getattr(default, name)
+
+
+class Cluster(object):
 
     def __init__(self, config):
         if isinstance(config, str):
@@ -35,5 +113,4 @@ class Bot(object):
                 config = json.loads(f)
             logging.info("Configured from %s." % config)
         self.config = config
-        # self.acesses = map(lambda c: Access(c), config['oauth'])
-        self.access = [Access(config['oauth'][0])]
+        self.bots = map(lambda c: Bot(c), config.items())
--- populartwitterbot/limits.py
+++ populartwitterbot/limits.py
@@ -1,46 +1,30 @@
-from queuelib import FifoDiskQueue
 from datetime import datetime
 import threading
 import logging
-import pickle
 from helpers import twython
 
 
 class LimitManager(object):
 
-    def __init__(self, access, enabled):
-        self.access = access
-        self.api = access.twitter
+    def __init__(self):
+        self.registered = []
         self.response_limits = {}
-        self.synchronize_limits()
-        self.initialize_queues()
-        self.wrap(self.api, enabled)
 
-    @property
-    def limits(self):
-        return LimitManager.limits
+    def register(self, access):
+        if not self.registered:
+            self.synchronize_limits(access)
+        self.registered.append(access)
 
-    @limits.setter
-    def limits(self, value):
-        # The limits are shared between all the instances.
-        # Maybe there is a need for a Lock when set or get.
-        LimitManager.limits = value
-
-    def initialize_queues(self):
-        self.qfactory = (lambda endpoint:
-                         FifoDiskQueue('./collections/%s' %
-                                       endpoint.replace('/', '+')))
-        self.queues = dict(map(lambda k: (k, self.qfactory(k)),
-                               self.limits.keys()))
-        self.priority_order = self.queues.keys()
+    def bootup_engine(self):
         self.engine = threading.Thread(target=self.engine_loop)
         self.running = True
         self.engine.start()
 
-    def synchronize_limits(self):
-        limits = self.api.get_application_rate_limit_status()
-        with open('limits.json', 'wb') as f:
-            pickle.dump(limits, f)
+    def keys(self):
+        return self.limits.keys()
+
+    def synchronize_limits(self, access):
+        limits = access.get_application_rate_limit_status()
         self.limits = limits['resources']
         super_dict = {}
         for d in self.limits.values():
@@ -52,7 +36,8 @@ class LimitManager(object):
 
     def engine_loop(self):
         while self.running:
-            self.unqueue_endpoints()
+            for access in self.registered:
+                self.unqueue_endpoints(access)
 
     def has_available_limit(self, queue_name):
         if queue_name in self.limits:
@@ -60,70 +45,43 @@ class LimitManager(object):
         elif queue_name in self.response_limits:
             limit = self.response_limits[queue_name]
         else:
-            limit = {'remaining': 1}
-        return (limit['remaining'] > 0 or
-                datetime.fromtimestamp(limit['reset']) < datetime.now())
+            limit = {u'remaining': 1,
+                     u'reset': int(datetime.now().strftime('%s'))}
+        return ((limit[u'remaining'] and limit[u'remaining'] > 0) or
+                (limit[u'reset'] and
+                 datetime.fromtimestamp(float(limit[u'reset'])) <
+                 datetime.now()))
 
     @twython
-    def send(self, request):
+    def send(self, access, request):
+        access.send(request)
         queue_name = request[1]
-        getattr(self, '_%s' % request[0])(
-            request[1],
-            request[2],
-            request[3])
         if queue_name in self.limits:
             self.limits[queue_name][u'remaining'] -= 1
-        h_get = self.api.get_lastfunction_header
+        h_get = access.get_lastfunction_header
         header = lambda k: int(h_get(k)) if h_get(k) else h_get(k)
-        self.response_limits[queue_name] = {
-            "limit": header('x-rate-limit-limit'),
-            "remaining": header('x-rate-limit-remaining'),
-            "class": header('x-rate-limit-class'),
-            "reset": header('x-rate-limit-reset')
+        temporal = {
+            u"limit": header('x-rate-limit-limit'),
+            u"remaining": header('x-rate-limit-remaining'),
+            u"class": header('x-rate-limit-class'),
+            u"reset": header('x-rate-limit-reset')
         }
-        print queue_name, self.response_limits[queue_name]
+        if None not in temporal.values():
+            self.response_limits[queue_name] = temporal
+        else:
+            print queue_name, temporal
 
-    def unqueue_endpoints(self):
+    def unqueue_endpoints(self, access):
         if self.next_sync < datetime.now():
-            self.synchronize_limits()
-        for queue_name in self.priority_order:
+            self.synchronize_limits(access)
+        for queue_name in access.priority_order:
             if self.has_available_limit(queue_name):
-                request = self.queues[queue_name].pop()
-                if request:
-                    unziped_request = pickle.loads(request)
-                    result = self.send(unziped_request)
+                unziped_request = access.pop(queue_name)
+                if unziped_request:
+                    result = self.send(access, unziped_request)
                     if not result:
-                        self.queues[queue_name].push(request)
+                        access.push(unziped_request)
 
-    def push(self, params):
-        endpoint = params[1]
-        if endpoint not in self.queues:
-            self.queues[endpoint] = self.qfactory(endpoint)
-            self.priority_order.append(endpoint)
-        self.queues[endpoint].push(pickle.dumps(params))
-
-    def get(self, endpoint, params=None, version='1.1'):
-        self.push(['get', endpoint, params, version])
-
-    def post(self, endpoint, params=None, version='1.1'):
-        self.push(['post', endpoint, params, version])
-
-    def fake_call(self, *args, **kwargs):
-        logging.info(args, kwargs)
-
-    def wrap(self, api, enabled):
-        self._get = api.get if enabled else self.fake_call
-        self._post = api.post if enabled else self.fake_call
-        api.get = self.get
-        api.post = self.post
-
-    def unwrap(self, api):
-        api.get = self._get
-        api.post = self._post
-
-    def shutdown(self):
+    def shutdown_engine(self):
         self.running = False
         self.engine.join()
-        for queue in self.queues.values():
-            queue.close()
-        self.unwrap(self.api)
