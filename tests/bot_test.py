6b3594d4bff5c2e5527192dace59b254d85cdf3f
eloy <eloy.colell@gmail.com>
2015-08-06 10:29:43 -0300

    It try to fix the unittest.

--- tests/bot_test.py
+++ tests/bot_test.py
@@ -10,27 +10,20 @@ import os
 class TestBot(unittest.TestCase):
 
     def setUp(self):
-        config = ("bot", {
-            "oauth": [
-                {"app_name": "",
-                 "api_key": "",
-                 "api_secret": "",
-                 "access": "",
-                 "access_secret": "",
-                 "enabled": False
-                 }
-            ]
-        })
         if os.path.exists('config.json'):
             with open('config.json') as f:
                 config = json.load(f).items()[0]
+        if 'CONFIG' in os.environ:
+            config = json.loads(os.environ['CONFIG']).items()[0]
         self.bot = Bot(config)
         self.text = 'Testing from github.com/limiear/populartwitterbot.'
         self.text += (" (%s)" % hashlib.md5(str(datetime.now())).
                       digest().encode("base64")[0:5].lower())
 
     def test_name(self):
-        # self.assertEquals(self.bot.name, 'populartwitterbot00')
+        self.assertEquals(self.bot.name, 'solarbot')
+        self.assertIn('populartwitterbot00', self.bot.access.keys())
+        self.assertIn('populartwitterbot01', self.bot.access.keys())
         self.bot.populartwitterbot00.update_status(status=self.text)
         time.sleep(10)
 
